acfea436808c9230f63aff63df6e738ee6aa8a93
sampath kumar <sampath.surineni@gmail.com>
2013-10-20 16:12:47 +0530

    added button and method

--- src/com/example/exp1/MainActivity.java
+++ src/com/example/exp1/MainActivity.java
@@ -1,17 +1,35 @@
 package com.example.exp1;
 
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	//Button fon=(Button)findViewById(R.id.click);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startLis();
+        
     }
 
+    void startLis(){
+    	Button fon=(Button)findViewById(R.id.click);
+    	fon.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
